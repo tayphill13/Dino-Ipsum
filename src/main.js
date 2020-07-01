5da61b679edd342bc2aac60945e1448b5aa1202b
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-01 15:20:09 -0700

    complete main.js with displays
    Co-Authored-By: co-authored-by: Ian Scott <chesnekov@gmail.com>
    co-authored-by: Ian Scott <chesnekov@gmail.com>
    Co-Authored-By: Peter Grimm <pagrimm@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,8 +5,41 @@ import './styles.css';
 import {Game} from './dino.js';
 
 $(document).ready(function () {
-  // $('#submit-button').submit(function (event) {
-  //     event.preventDefault();
-
-  // });
+  let game = new Game();
+  (async () => {
+    await game.getWord();
+    displayGame(game);
+  });
+  
+  $('#submit-button').submit(function (event) {
+    event.preventDefault();
+    let input = $("#letter-input").val();
+    game.checkLetter(input);
+    displayGame(game);
+  });
 });
+
+function displayGame(game) {
+  displayWord(game);
+  displayGuesses(game);
+  displayGuessedLetters(game);
+}
+
+function displayWord (game) {
+  $("#dino-word").text("");
+  for (let i = 0; i < game.word.length; i++) {
+    if (game.word[i].guessed) {
+      $("#dino-word").append(game.word[i].letter);
+    } else {
+      $("#dino-word").append("_");
+    }
+  }
+}
+
+function displayGuesses (game) {
+  $("#guesses-remain").text(game.guesses);
+}
+
+function displayGuessedLetters (game) {
+  $("#already-guessed").text(game.inputs.join(", "));
+}
