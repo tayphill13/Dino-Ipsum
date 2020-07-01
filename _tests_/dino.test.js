4cf217d00b26bc0e374f915801d80205ec49310a
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-01 10:49:53 -0700

    show a good failed test for our API call of dino word
    co-authored-by: Ian Scott <chesnekov@gmail.com>
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>

--- _tests_/dino.test.js
+++ _tests_/dino.test.js
@@ -1,16 +1,20 @@
 import {Game} from '../src/dino.js';
 
 describe ('dino hangman', () => {
+  let game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
 
   test("Should correctly create game object with word array, input letters array, and remaining guesses", () => {
-    let game = new Game();
     expect(game.word).toEqual([]);
     expect(game.inputs).toEqual([]);
     expect(game.guesses).toEqual(7);
   });
 
-  test("", () => {
-
+  test("Should correctly populate word array with API call", () => {
+    expect(game.word.length).toBeGreaterThan(0);
   });
 
 });
